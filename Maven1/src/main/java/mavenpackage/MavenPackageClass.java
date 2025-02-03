b071aa15a438bd17d6dbae31825067e809c9e176
LENOVO <LENOVO@192.168.1.37>
2025-02-03 10:13:32 +0530

    2nd Commit

---
+++ Maven1/src/main/java/mavenpackage/MavenPackageClass.java
@@ -0,0 +1,10 @@
+package mavenpackage;
+
+public class MavenPackageClass {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+String a = "hello";
+System.out.println(a);	}
+
+}
